314251dd45a4c2254152199bde73ef209cb5caf8
domhanak <domin.hanak@gmail.com>
2015-10-28 10:35:30 +0100

    milestone01: add EmbeddedDatabase ultity with persistence config

---
+++ src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/utils/PokemonLeagueRunner.java
@@ -0,0 +1,19 @@
+package cz.fi.muni.pa165.seminar.pkmnleague.utils;
+
+import java.sql.SQLException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Runner for our application.
+ *
+ * @author Dominik Hanak @domhanak
+ */
+@SpringBootApplication
+@ImportResource("classpath:/dao-context.xml")
+public class PokemonLeagueRunner {
+    public static void main(String[] args) throws SQLException {
+        SpringApplication.run(PokemonLeagueRunner.class, args);
+    }
+}
