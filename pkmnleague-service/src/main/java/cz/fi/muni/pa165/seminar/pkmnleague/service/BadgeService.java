8d3f302ac7044e0d585ce19ea77da4fef9dc55fe
Pavel Kouřil <pk@pavelkouril.cz>
2015-11-24 10:26:28 +0100

    Service layer: added basic methods to interfaces

--- pkmnleague-service/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/service/BadgeService.java
+++ pkmnleague-service/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/service/BadgeService.java
@@ -1,7 +1,14 @@
 package cz.fi.muni.pa165.seminar.pkmnleague.service;
 
+import cz.fi.muni.pa165.seminar.pkmnleague.domain.Badge;
+
+import java.util.List;
+
 /**
  * @author Pavel Kouřil <pk@pavelkouril.cz>
  */
 public interface BadgeService {
+    Badge findById(int id);
+    List<Badge> findAll();
+    void delete(Badge gym);
 }
--- pkmnleague-service/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/service/GymService.java
+++ pkmnleague-service/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/service/GymService.java
@@ -1,7 +1,14 @@
 package cz.fi.muni.pa165.seminar.pkmnleague.service;
 
+import cz.fi.muni.pa165.seminar.pkmnleague.domain.Gym;
+
+import java.util.List;
+
 /**
  * @author Pavel Kouřil <pk@pavelkouril.cz>
  */
 public interface GymService {
+    Gym findById(int id);
+    List<Gym> findAll();
+    void delete(Gym gym);
 }
