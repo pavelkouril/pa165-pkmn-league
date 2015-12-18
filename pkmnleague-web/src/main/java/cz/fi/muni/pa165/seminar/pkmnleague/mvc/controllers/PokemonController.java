5b04110d2479394f8310384b13c86ef6f2416fef
domhanak <domin.hanak@gmail.com>
2015-12-18 11:47:24 +0100

    milestone03: add views for Gym and Pokemon

--- pkmnleague-web/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/mvc/controllers/PokemonController.java
+++ pkmnleague-web/src/main/java/cz/fi/muni/pa165/seminar/pkmnleague/mvc/controllers/PokemonController.java
@@ -1,9 +1,7 @@
 package cz.fi.muni.pa165.seminar.pkmnleague.mvc.controllers;
 
 import cz.fi.muni.pa165.seminar.pkmnleague.dto.PokemonCreateDTO;
-import cz.fi.muni.pa165.seminar.pkmnleague.dto.PokemonDTO;
 import cz.fi.muni.pa165.seminar.pkmnleague.facade.PokemonFacade;
-import cz.fi.muni.pa165.seminar.pkmnleague.mvc.exceptions.UnexisitngReourceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
